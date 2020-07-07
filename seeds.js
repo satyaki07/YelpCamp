3117eee505adde5ae929f316abd2a63138c2548f
Satyaki Bose <satyakibose98@gmail.com>
2020-07-07 16:04:23 +0530

    Add Password reset with Node-mailer

--- seeds.js
+++ seeds.js
@@ -1,58 +1,48 @@
 var mongoose = require("mongoose");
 var Campground = require("./models/campground");
-var Comment   = require("./models/comment");
+var Comment = require("./models/comment");
 
-var data = [
+var seeds = [
     {
-        name: "Cloud's Rest", 
+        name: "Cloud's Rest",
         image: "https://farm4.staticflickr.com/3795/10131087094_c1c0a1c859.jpg",
         description: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy."
     },
     {
-        name: "Desert Mesa", 
+        name: "Desert Mesa",
         image: "https://images.unsplash.com/photo-1542067519-6cd1e217df2d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80",
         description: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy."
     },
     {
-        name: "Canyon Floor", 
+        name: "Canyon Floor",
         image: "https://farm1.staticflickr.com/189/493046463_841a18169e.jpg",
         description: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy."
     }
-]
+];
 
-function seedDB(){
-   //Remove all campgrounds
-   Campground.remove({}, function(err){
-        if(err){
-            console.log(err);
-        }
-        console.log("removed campgrounds!");
-         //add a few campgrounds
-        data.forEach(function(seed){
-            Campground.create(seed, function(err, campground){
-                if(err){
-                    console.log(err)
-                } else {
-                    console.log("added a campground");
-                    //create a comment
-                    Comment.create(
-                        {
-                            text: "This place is great, but I wish there was internet",
-                            author: "Homer"
-                        }, function(err, comment){
-                            if(err){
-                                console.log(err);
-                            } else {
-                                campground.comments.push(comment);
-                                campground.save();
-                                console.log("Created new comment");
-                            }
-                        });
-                }
+async function seedDB() {
+    try {
+        await Campground.remove({});
+        console.log("Campgrounds removed");
+        await Comment.remove({});
+        console.log('Comments removed');
+
+        for (const seed in seeds) {
+            let campground = await Campground.create(seed);
+            console.log('Campground created');
+            let comment = await Comment.create({
+                text: 'Great Place!',
+                author: 'Homer'
             });
-        });
-    }); 
-    
+            console.log('Comment created');
+            campground.comments.push(comment);
+            campground.save();
+            console.log('Comment added to the Campground');
+        }
+    } catch (err) {
+        console.log(err);
+    }
+
 }
 
 module.exports = seedDB;
